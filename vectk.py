5175a2e827f9282fb317c37fa7201684feec94be
DedkovEA <dedkov.ea@phystech.edu>
2019-10-08 11:40:19 +0300

    Some changes to get closer to PEP-8

--- vectk.py
+++ vectk.py
@@ -8,13 +8,15 @@ Size = im.size
 f = open('pixsvec.py', 'w')
 
 f.write('import tkinter\nroot = tkinter.Tk()\ncanvas = tkinter.Canvas(root)\n')
-f.write('canvas["width"] = '+str(Size[0])+'\ncanvas["height"] = '+str(Size[1])+'\n')
+f.write('canvas["width"] = ' + str(Size[0]) + '\ncanvas["height"] = ' + str(Size[1]) + '\n')
 for i in range(Size[1]):
     x_start = 0
     for j in range(Size[0]):
-        if pic[x_start,i] != pic[j,i]:
-            f.write('canvas.create_line('+str(x_start)+','+str(i)+','+str(j-1)+','+str(i)+','+str()+'fill = "'+('#%02x%02x%02x' % (pic[x_start, i]))+'")\n') 
+        if pic[x_start, i] != pic[j, i]:
+            f.write('canvas.create_line(' + str(x_start) + ',' + str(i) + ',' + str(j - 1) + ',' + str(
+                i) + ',' + str() + 'fill = "' + ('#%02x%02x%02x' % (pic[x_start, i])) + '")\n')
             x_start = j
-    f.write('canvas.create_line('+str(x_start)+','+str(i)+','+str(j-1)+','+str(i)+','+str()+'fill = "'+('#%02x%02x%02x' % (pic[x_start, i]))+'")\n') 
-        
+    f.write('canvas.create_line(' + str(x_start) + ',' + str(i) + ',' + str(j - 1) + ',' + str(
+        i) + ',' + str() + 'fill = "' + ('#%02x%02x%02x' % (pic[x_start, i])) + '")\n')
+
 f.write('canvas.pack()\ntkinter.mainloop()')
